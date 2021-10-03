679c9baf2e999f1aa2b39200a4c65da46b75b963
Fasory <72564803+Fasory@users.noreply.github.com>
2021-10-03 15:05:17 +0200

    v0.0.4	-	mise en place du menu be base

--- src/vue/MenuPrincipal.java
+++ src/vue/MenuPrincipal.java
@@ -1,6 +1,7 @@
 package vue;
 
-import javax.swing.JPanel;
+import javax.swing.*;
+import java.awt.*;
 
 import controleur.Controleur;
 
@@ -11,5 +12,33 @@ public class MenuPrincipal extends JPanel {
 	MenuPrincipal(Controleur controleur) {
 		super();
 		this.controleur = controleur;
+		
+		
+		// Partie Affichage
+		setLayout(new GridBagLayout());
+		
+		JPanel panelBtn = new JPanel(new GridLayout(5, 1, 0, 40)) {
+			@Override
+			public Dimension getPreferredSize() {
+				return new Dimension(200, 350);
+			}
+		};
+		
+		JButton btnCreerPartie = new JButton("Créer une nouvelle partie");
+		panelBtn.add(btnCreerPartie);
+		
+		JButton btnSelectionnerPartie = new JButton("Sélectionner une partie");
+		panelBtn.add(btnSelectionnerPartie);
+		
+		JButton btnOption = new JButton("Options");
+		panelBtn.add(btnOption);
+		
+		JButton btnScore = new JButton("Voir les scores");
+		panelBtn.add(btnScore);
+		
+		JButton btnQuitter = new JButton("Quitter");
+		panelBtn.add(btnQuitter);
+		
+		add(panelBtn);
 	}
 }
